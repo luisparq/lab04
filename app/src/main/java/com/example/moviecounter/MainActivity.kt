20956f165445ac037286ee0d90907d2114d272a9
luisparq <luis.parqui@tecsup.edu.pe>
2026-09-15 16:45:44 -0500

    feat: Add MovieCard component

--- app/src/main/java/com/example/moviecounter/MainActivity.kt
+++ app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MovieCounterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MovieCard(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
---
+++ app/src/main/java/com/example/moviecounter/MovieCard.kt
@@ -0,0 +1,20 @@
+package com.example.moviecounter
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MovieCard(modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.padding(16.dp)
+    ) {
+        Text(
+            text = "Mi película favorita",
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
